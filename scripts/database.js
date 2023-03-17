f250fa4189fc1e5747ee3149687ea27d781d9821
Gloria Reina <gloria.l.reina11@gmail.com>
2023-03-17 10:52:37 -0400

    first merge

--- scripts/database.js
+++ scripts/database.js
@@ -1,4 +1,5 @@
-const database = {
+const database = {       
+
     transientState: {}
 }
 
@@ -12,8 +13,23 @@ export const getLocations = () => {
 }
 
 export const completeOrder = () => {
+    const newOrder = {...database.transientState}
 
-        // Broadcast custom event to entire documement so that the
+    // Add a new primary key to the object
+    const lastIndex = database.customOrders.length - 1
+    newOrder.id = database.customOrders[lastIndex].id + 1
+
+    // Add a timestamp to the order
+    newOrder.timestamp = Date.now()
+
+    // Add the new order object to custom orders state
+    database.customOrders.push(newOrder)
+
+    // Reset the temporary state for user choices
+    database.transientState = {}
+
+    // Broadcast a notification that permanent state has changed
+        // Broadcast custom event to entire document so that the
         // application can re-render and update state
         document.dispatchEvent( new CustomEvent("stateChanged") )
 }
