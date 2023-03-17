c745f43a347c5c661a36b6bd6ebd79bcaa5d9e2c
Arnold Rispress <arnold.rispress@gmail.com>
2023-03-17 14:01:32 -0400

    adding code

--- scripts/yallHungry.js
+++ scripts/yallHungry.js
@@ -2,6 +2,8 @@ import { happyToy } from "./toys.js"
 import {Desserts} from "./dessert.js"
 import { addCustomOrder } from "./dataChange.js"
 import { Drinks } from "./Drinks.js"
+import { Locations } from "./Locations.js"
+import { Foods } from "./Food.js"
 
 document.addEventListener(
     "click",
@@ -17,22 +19,22 @@ export const yallHungry= () => {
     return  `
         <h1>Yall Hungry?</h1>
         <article class="choices">
-            <section class="choices__ options">
-                <h2></h2>
-                ${()}
+            <section class="choices__locations options">
+                <h2>Locations</h2>
+                ${Locations()}
+            </section>
+            <section class="choices__foods options">
+                <h2>Food</h2>
+                ${Foods()}
             </section>
             <section class="choices__drinks options">
                 <h2>Drinks</h2>
                 ${Drinks()}
             </section>
+            </section>
             <section class="choices__desserts options">
                 <h2>Desserts</h2>
                 ${Desserts()}
-            </section>
-            </section>
-            <section class="choices__ options">
-                <h2></h2>
-                ${()}
             </section>
             <section class="choices__happyToys options">
                 <h2>Happy Toys!!!!</h2>
