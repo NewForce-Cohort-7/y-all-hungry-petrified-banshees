ae1e67153f5d3197ff69c95c992d572fd69dd698
Austin Davis <davis.austin1.1.c@gmail.com>
2023-03-17 10:27:11 -0400

    Completion of db/access/change/added main container

---
+++ scripts/dataAccess.js
@@ -0,0 +1,51 @@
+import { database } from "./database.js"
+
+//Create get functions
+export const getLocations = () => {
+    return database.locations.map(location => ({...location}))
+}
+export const getFoods = () => {
+    return database.food.map(food => ({...food}))
+}
+export const getDesserts = () => {
+    return database.desserts.map(dessert => ({...dessert}))
+}
+export const getToys = () => {
+    return database.happyToys.map(toy => ({...toy}))
+}
+export const getDrinks = () => {
+    return database.drinks.map(drink => ({...drink}))
+}
+export const getfoodInventory = () => {
+    return database.foodInventory.map(foodItems => ({...foodItems}))
+}
+export const getdrinkInventory = () => {
+    return database.drinkInventory.map(drinkItems => ({...drinkItems}))
+}
+export const getdessertInventory = () => {
+    return database.dessertInventory.map(dessertItems => ({...dessertItems}))
+}
+export const gethappyToysInventory = () => {
+    return database.happyToysInventory.map(toyItems => ({...toyItems}))
+}
+//Create set functions
+export const setLocation = (id) => {
+    database.orderBuilder.locationId = id
+    document.dispatchEvent(new CustomEvent("stateChanged"))
+}
+export const setFood = (id) => {
+    database.orderBuilder.foodId = id
+    document.dispatchEvent(new CustomEvent("stateChanged"))
+}
+export const setDrink = (id) => {
+    database.orderBuilder.drinkId = id
+    document.dispatchEvent(new CustomEvent("stateChanged"))
+}
+export const setDessert = (id) => {
+    database.orderBuilder.dessertId = id
+    document.dispatchEvent(new CustomEvent("stateChanged"))
+}
+export const setToy = (id) => {
+    database.orderBuilder.toyId = id
+    document.dispatchEvent(new CustomEvent("stateChanged"))
+}
