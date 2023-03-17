b4fcc6dd705ef325c68ed19e3fcff32709a3c005
Gloria Reina <gloria.l.reina11@gmail.com>
2023-03-17 11:26:33 -0400

    pause on progress to merge

---
+++ scripts/dessert.js
@@ -0,0 +1,32 @@
+import {setDessert, getDesserts } from "./dataAccess.js";
+
+
+desserts = getDesserts()
+
+//will need a addevent listeners
+document.addEventListener(
+    "change",
+    (changeEvent) => {
+        if (changeEvent.target.id === "dessert") {
+            const chosenOption = changeEvent.target.value
+            
+            setDessert(parseInt(changeEvent.target.value))  
+        }
+    }
+)
+
+//build html that will be exported to the module responsible for our html
+
+export const Desserts = () => {
+    let html = '<select id="desserts">'
+    html += '<option value="0"> Select a dessert choice</option>'
+
+    const arrayOfOptions = desserts.map( (dessert) => {
+            return `<option value="${dessert.id}">${dessert.name}</option>`
+        }
+    )
+
+    html += arrayOfOptions.join("")
+    html += "</select>"
+    return html
+}
