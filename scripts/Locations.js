9e0aae07dbd080b20b1aa8822ca15ff064fe0222
Arnold Rispress <arnold.rispress@gmail.com>
2023-03-17 14:32:10 -0400

    fixed code errors in locations

--- scripts/Locations.js
+++ scripts/Locations.js
@@ -13,14 +13,15 @@ document.addEventListener(
 )
 
 export const Locations = () => {
-    let html;
+    
 
-    html += '<select id ="location">'
+    let html = '<select id ="location">'
     html += `<option value="0">Select a location</option>`
 
     for (const location of locations) {
         html += `<option value="${location.id}">${location.name}`
     }
+    
     html += "</select>"
     return html
 }
