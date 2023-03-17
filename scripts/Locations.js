1b6abf47bc793ff2cbd7eddcfea8d89070860232
Arnold Rispress <arnold.rispress@gmail.com>
2023-03-17 11:26:46 -0400

    adding code

--- scripts/Locations.js
+++ scripts/Locations.js
@@ -1,12 +1,26 @@
-import { getLocations }
+import { getLocations, setLocation } from ".dataAccess.js"
+
+const locations = getLocations()
 
 document.addEventListener(
     "change",
     (event) => {
         if (event.target.name === "location") {
             setLocation(parseInt(event.target.value))
-            // window.alert(`User chose size ${event.target.value}`)
+            
         }
     }
 )
 
+export const Locations = () => {
+    let html = "<h2>Choose a location to pick up your order</h2>"
+
+    html += '<select id ="location">'
+    html += `<option value="0">Select a location</option>`
+
+    for (const location of locations) {
+        html += `<option value="${location.id}">${location.name}`
+    }
+    html += "</select>"
+    return html
+}
