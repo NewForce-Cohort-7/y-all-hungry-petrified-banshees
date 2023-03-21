834a5d7b57b85616efe093d70d25111a6b6c9cda
Austin Davis <davis.austin1.1.c@gmail.com>
2023-03-21 15:20:38 -0400

    update to css styling and addition of location tag and corrections

--- scripts/Locations.js
+++ scripts/Locations.js
@@ -1,23 +1,16 @@
-import { getLocations, setLocation, getOrderBuilder } from "./dataAccess.js"
+import { getLocations, setLocation, getOrderBuilder, getOrders } from "./dataAccess.js"
 
 const locations = getLocations()
 
-document.addEventListener(
-    "change",
-    (event) => {
-        if (event.target.id === "location") {
-            setLocation(parseInt(event.target.value))
-            
-        }
-    }
-)
+
+
 
 export const Locations = () => {
     const currentOrder = getOrderBuilder()
-
+    
     let html = '<select id="location">'
     html += currentOrder.locationId ? `<option value="0">Select a location`: `<option value="0" selected>Select a location`
-       
+    
     
     for (const location of locations) {
         if(currentOrder.locationId === location.id){
@@ -29,3 +22,25 @@ export const Locations = () => {
     html += "</select>"
     return html
 }
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.id === "location") {
+            setLocation(parseInt(event.target.value))  
+        }
+
+        const order = getOrderBuilder()
+        let locationMatch;
+        for (const location of locations) {
+            if(location.id === order.locationId){
+                locationMatch = location.name
+            }
+        }
+        if (locationMatch !== null) {
+            document.querySelector('#order-location').innerHTML = `You're picking up your order at our <strong>${locationMatch}</strong> location!`
+        }
+        //if null, order-location is blank
+        else { document.querySelector('#order-location').innerHTML = '' }
+    }
+)
