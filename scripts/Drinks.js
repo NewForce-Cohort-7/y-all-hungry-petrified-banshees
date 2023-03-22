548c08368a4d058ea871114132e643f203ae377e
Spencer Lott <spklott@gmail.com>
2023-03-22 11:22:53 -0400

    getting ready to merge

--- scripts/Drinks.js
+++ scripts/Drinks.js
@@ -5,14 +5,36 @@ const drinksArray = getdrinkInventory()
 document.addEventListener(
     "change",
     (event) => {
-        if (event.target.id === "singleDrink") {
-            // const chosenOption = event.target.value
-            // console.log(chosenOption)  // "1" or "2"
 
+        if (event.target.id === "singleDrink") {
             setDrink(parseInt(event.target.value))
         }
+       drinkSubtotal()
     }
 )
+export const drinkSubtotal = () => {
+    let drinkMatch = {}
+    const order = getOrderBuilder()
+    for (const drink of drinks) {
+        if(drink.id === order.drinkId){
+            drinkMatch = drink
+        }
+    }
+    const drinkPrice = drinkMatch.price
+    const drinkCostString = drinkPrice.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    })
+    // console.log(drinkCostString)
+    // return drinkCostString
+    if(drinkMatch !== null){
+        document.querySelector("#total").innerHTML = `Subtotal: <strong>${drinkCostString}</strong>`
+        
+    }
+
+    else{document.querySelector("#total").innerHTML = ''}
+     
+}
 
 
 export const Drinks = () => {
@@ -42,18 +64,3 @@ export const Drinks = () => {
         </select>
     `
         }
-//experimenting for a subtotal
-        // const RunningSubtotal = (drinksArray) => {
-        //     let total = 0
-        //     let result = []
-        
-        //     for (){
-        //         total = total + drinksArray[i]
-        //         result.push(total)
-        //     }
-        //     return `<article class="Subtotal">
-        //                 <div> ${result} </div>
-        //                  </article>`
-        
-        // }
-        // RunningSubtotal()
